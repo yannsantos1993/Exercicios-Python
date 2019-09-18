df66b0f5229332202adea25b5332331a99ac8cbf
unknown <TI-Turma-B@EP-LABINFO02-86.santoagostinho.com.br>
2019-09-18 19:39:10 -0300

    Exercícios

---
+++ Lista 04/Exercício 02.py
@@ -0,0 +1,4 @@
+''' Leia a idade, a altura e o peso de 3 pessoas. Calcule e mostre:
+a) A quantidade de pessoas com idade superior a 20 anos;
+b) A média das alturas das pessoas com idade entre 15 e 18 anos;
+c) A porcentagem de pessoas com peso inferior a 60 quilos entre todas as pessoas analisadas.'''
