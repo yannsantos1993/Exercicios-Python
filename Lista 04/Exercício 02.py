c373bb9485ac971bbdc9c0cc30970b14b71abc92
unknown <TI-Turma-B@EP-LABINFO02-86.santoagostinho.com.br>
2019-09-18 21:57:51 -0300

    Exercicios

--- Lista 04/Exercício 02.py
+++ Lista 04/Exercício 02.py
@@ -31,4 +31,28 @@ else:
     I3 = 0
 
 TOTAL_PESSOAS20 = I1+I2+I3
-print("Das pessoas informadas", TOTAL_PESSOAS20, "tem idade superior a 20 anos")
+print("Das pessoas informadas", TOTAL_PESSOAS20, "tem idade superior a 20 anos.")
+
+if IDADE_1 >= 15 and IDADE_1 <= 18:
+    A1 = ALTURA_1
+    A11 = 1
+else:
+    A1 = 0
+    A11 = 0
+
+if IDADE_2 >= 15 and IDADE_2 <= 18:
+    A2 = ALTURA_2
+    A22 = 1
+else:
+    A2 = 0
+    A22 = 0
+
+if IDADE_3 >= 15 and IDADE_3 <= 18:
+    A3 = ALTURA_3
+    A33 = 1
+else:
+    A3 = 0
+    A33 = 0
+
+MEDIA_ALTURAS = (A1 + A2 + A3) / (A11 + A22 + A33)
+print ("A Média de altura das pessoas de faixa etária dentre 15 e 18 anos é de", MEDIA_ALTURAS, "m.")
