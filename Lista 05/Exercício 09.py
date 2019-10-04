4d728418fd12f15eb37cd0e000124d24e8a0b40d
unknown <TI-Turma-B@EP-LABINFO02-86.santoagostinho.com.br>
2019-10-04 20:50:17 -0300

    Lista nova

--- Lista 05/Exercício 09.py
+++ Lista 05/Exercício 09.py
@@ -1,33 +1,36 @@
 NUM = int(input("Digite um número entre 0 e 9 para que possamos adivinhar o número que você escolher:"))
-if NUM == 0:
-    N = "Zero"
+if NUM <0 or NUM >9:
+    print("Por favor escolha apenas um número entre 0 e 9")
 else:
-    if NUM >0 and NUM <=1:
-        N = "Um"
+    if NUM == 0:
+        N = "ZERO"
     else:
-        if NUM > 1 and NUM <= 2:
-            N = "Dois"
+        if NUM >0 and NUM <=1:
+            N = "UM"
         else:
-            if NUM > 2 and NUM <= 3:
-                N = "Três"
+            if NUM > 1 and NUM <= 2:
+                N = "DOIS"
             else:
-                if NUM > 3 and NUM <= 4:
-                    N = "Quatro"
+                if NUM > 2 and NUM <= 3:
+                    N = "TRÊS"
                 else:
-                    if NUM > 4 and NUM <= 5:
-                        N = "Cinco"
+                    if NUM > 3 and NUM <= 4:
+                        N = "QUATRO"
                     else:
-                        if NUM > 5 and NUM <= 6:
-                            N = "Seis"
+                        if NUM > 4 and NUM <= 5:
+                            N = "CINCO"
                         else:
-                            if NUM > 6 and NUM <= 7:
-                                N = "Sete"
+                            if NUM > 5 and NUM <= 6:
+                                N = "SEIS"
                             else:
-                                if NUM > 7 and NUM <= 8:
-                                    N = "oito"
+                                if NUM > 6 and NUM <= 7:
+                                    N = "SETE"
                                 else:
-                                    if NUM > 8 and NUM <= 9:
-                                        N = "nove"
+                                    if NUM > 7 and NUM <= 8:
+                                        N = "OITO"
                                     else:
-                                        None
-print(f"Com base em adivinhação, ousamos em dizer que o número escolhido foi {N} !!!")
+                                        if NUM > 8 and NUM <= 9:
+                                            N = "NOVE"
+                                        else:
+                                            None
+    print(f"Com base em adivinhação, ousamos em dizer que o número escolhido foi {N} !!!")
