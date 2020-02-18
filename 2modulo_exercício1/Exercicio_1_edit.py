e88f670a862996d4bd8b4e3954a6942e799abc37
Aluno TI-B <alunoti-b@epsa.com.br>
2020-02-18 19:49:39 -0300

    updates

---
+++ 2modulo_exercício1/Exercicio_1_edit.py
@@ -0,0 +1,16 @@
+codigos = []
+precos = []
+contador = 0
+ocorrencias = 3
+
+
+while contador < ocorrencias:
+    codigos.append(int(input("Digite o código do produto: ")))
+    precos(float(input("Digite o preço do produto:")))
+    contador +=1
+
+menor_preco = precos.index(min(precos))
+
+print(f"O maior preço é: {max(precos)}")
+print(f"A média dos preços dos produtos listados equivale a {sum(precos)/ocorrencias}")
+print(f"O código do produto mais barato é {codigos[menor_preco]}")
